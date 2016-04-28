4203148e7ea1ce9066e58d5d27b323278de38f12
Dmgito <davidmartin_75@hotmail.com>
2016-04-28 20:30:40 +0200

    Correcciones tema 5
    ok

---
+++ Tema 5/Correcciones otros/xcsdty/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AnimatorStateInfo currentBaseState = anim.GetCurrentAnimatorStateInfo(0);
+		if (currentBaseState.IsName("Idle")  && Input.GetKeyDown (KeyCode.J)) {
+			anim.SetTrigger ("shootCamera");
+		}
+	}
+}
