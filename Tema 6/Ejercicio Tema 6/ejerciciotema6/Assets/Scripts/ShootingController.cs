21607c4b34bb2f58e0d17fd93404977fdc7eb063
Dmgito <davidmartin_75@hotmail.com>
2016-04-30 23:02:34 +0200

    Tema 6
    Entregable y ejercicio práctico tema 6

---
+++ Tema 6/Ejercicio Tema 6/ejerciciotema6/Assets/Scripts/ShootingController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingController : MonoBehaviour {
+
+    public Transform proyectil;
+    public Transform posicionInicialBala;
+    public int velocidadDisparo = 2;
+
+	public Transform enemigo; 
+	private MovimientoEnemigo scriptEnemigo; 
+	// Use this for initialization
+	void Start () {
+		scriptEnemigo =	enemigo.GetComponent<MovimientoEnemigo>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Transform bala = (Transform)Instantiate(proyectil, posicionInicialBala.position, transform.rotation);
+            bala.GetComponent<Rigidbody>().AddForce(this.transform.forward * 1000 * velocidadDisparo);
+            Destroy(bala.gameObject, 5);
+
+			scriptEnemigo.setPersiguiendo(true);
+			scriptEnemigo.cambiarObjetivo(transform);
+
+        }
+	}
+}
---
+++ Tema 6/entregable6/Validarentreg/Assets/Scripts/DetectarPlayerPasillo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectarPlayerPasillo : MonoBehaviour {
+
+	public Transform miEnemigoVerde;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			Ataque script = miEnemigoVerde.GetComponent<Ataque>();
+			script.Atacar();
+		}
+	}
+}
