3f9caadc422df666932236011fd439ab4eacc2ab
Dmgito <davidmartin_75@hotmail.com>
2016-05-15 18:25:58 +0200

    Entregable7
    Entregable 7 y pruebas

---
+++ Tema 7/Entregable 7/Entregable 7/Assets/Scripts/PasarAEscena2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PasarAEscena2 : MonoBehaviour {
+
+	public void AbrirEscena2 ()
+	{
+		Application.LoadLevel("Escena2");
+	}
+		
+
+}
---
+++ Tema 7/ValidarEntregable/Validar/Assets/Scripts/PasarEscenaInicial.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PasarEscenaInicial : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.V))
+		{
+			Application.LoadLevel("EscenaInicial");
+		}
+	}
+}
